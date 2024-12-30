16f2cdf86bb59fa3711929eef3a788982ed05997
Velka-DEV <git@stackblitz.com>
2024-12-30 13:55:11 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  nitro: {
+    preset: 'cloudflare-pages',
+  },
+  devtools: { enabled: true },
+  modules: ['shadcn-nuxt', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
+});
